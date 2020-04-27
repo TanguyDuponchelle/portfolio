defe916da11cfcdd9065dda6c8ddd9f5ed9f7899
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2020-04-27 21:53:15 +0200

    setup early gatsby: index, header & layout components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import Header from "../components/Header"
+import Layout from "../components/Layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Layout>
+      <Header headerText="Tanguy Duponchelle Portfolio" />
+      <h1>Main projects</h1>
+    </Layout>
+  </div>
+)
